8ae009cdcfe80a8e12d59290ffcc24652345b04d
Richard Kim <richeekim@gmail.com>
2018-01-25 00:06:39 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+function theBeatlesPlay(musicians, instruments) {
+  var result = [];
+  var i
+    for (i=0; i<musicians.length; i++) {
+        result[i] = musicians[i] + " plays " + instruments[i]}
+    return result;
+}
+function johnLennonFacts(facts) {
+  var i = 0
+  while (i<facts.length) {
+    facts[i] = facts[i] + "!!!";
+    i++;
+  }
+  return facts;
+}
